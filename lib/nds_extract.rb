79fe4095252abbd161bd775d2fa0e3796ef254a3
David Holmes <phoenix1988@gmail.com>
2020-01-01 21:13:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -68,10 +68,11 @@ def gross_per_studio(collection)
   i = 0
   final = {}
   while i < collection.length
-    if collection[i][:studio] != final[collection[i][:studio]]
-      final[collection[i][:studio]] => collection[i][:worldwide_gross]
+    studio = collection[i][:studio]
+    if final[studio]
+      final[studio] => final[studio] + collection[i][:worldwide_gross]
     else
-      final[collection[i][:studio]] => final[collection[i][:studio]] + collection[i][:worldwide_gross]
+      final[studio] => collection[i][:worldwide_gross]
     end
     i += 1
   end
